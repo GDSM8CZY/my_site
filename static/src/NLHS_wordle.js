63fa971ed3cbcbe2faf4b1b00f16a5d9675650af
Aditya Kale <adityaraokale@gmail.com>
2025-11-14 19:31:22 -0500

    Cells align center, can check guesses

--- static/src/NLHS_wordle.js
+++ static/src/NLHS_wordle.js
@@ -1,11 +1,18 @@
-const cells = document.getElementsByClassName("cell")
+const { append } = require("express/lib/response");
+
+const cells = document.getElementsByClassName("cell");
+
+let row_num = 0;
 
 fetch("/NLHS_wordle/get_daily_word")
     .then(res => res.json())
-    .then(data => console.log(data.word))
 
-function test() {
-    for (let i = 0; i <= cells.length; i++) {
-        cells[i].innerHTML = "no";
+function check_guess() {
+    const row = document.getElementById(row_num);
+    let guess = "";
+    for (let i = 0; i < row.children.length; i++) {
+        guess += row.children[i].value;
     }
+    console.log(guess);
+    row_num++;
 }
